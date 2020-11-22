46f5c4251a5a293d2b624c8094a7503debe92f14
joeyrebbe <josephrebbe@gmail.com>
2020-11-21 20:59:07 -0800

    add auth route to createUser

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,68 +1,33 @@
-const { User, TravelLog } = require('../db/schema')
-const jwt = require('jsonwebtoken')
-const { checkPassword, generatePassword } = require('../middleware/PasswordHandler')
+const { User } = require('../db/schema')
+const passport = require('../middleware/passportConfig')
+const { generatePassword } = require('../middleware/PasswordHandler')
 
-const GetProfile = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.user_id)
-    res.render('profile', { user })
-  } catch (error) {
-    throw error
-  }
-}
-
-const CreateUser = async (req, res) => {
-  try {
-    const body = req.body
-    const password = await generatePassword(body.password)
-    const user = new User({
-      username: body.username,
-      email: body.email,
-      password
-    })
-    user.save()
-    res.render('login')
-  } catch (error) {
-    throw error
-  }
-}
-
-const SignInUser = async (req, res, next) => {
-  try {
-    const user = await User.findOne({ email: req.body.email })
-    if (
-      user &&
-      (await checkPassword(req.body.password, user.password))
-    ) {
-      const payload = {
-        _id: user._id,
-        username: user.username,
-        artistname:user.artistname,
-        profilepic: user.profilepic, 
-        coverpic: user.coverpic,
-        links: user.links
-      }
-      res.locals.payload = payload
-      return next()
+const CreateUser = async (req, res, next) => {
+    console.log('REQUEST BODY', req.body)
+    try {
+      const body = req.body
+      const password = await generatePassword(body.password)
+      // Passwords matched, so now we'll create a new user
+      const user = new User({
+              username: body.username,
+              email: body.email,
+              password
+            })
+      await user.save()
+      passport.authenticate('local', {
+          successFlash: 'Successful login, welcome.',
+          successRedirect: '/user/profile',
+          failureFlash: 'Invalid Credentials',
+          failureRedirect: 'user/login'
+      })(req, res, next)
     }
-    res.status(401).send({ msg: 'Unauthorized' })
-  } catch (error) {
-    throw error
-  }
+    catch(err)  {
+        console.log('Error creating a user', err)
+        res.redirect('/user/signup')
+    }
 }
 
-const RefreshSession = (req, res) => {
-  try {
-    const token = res.locals.token
-    res.send({ user: jwt.decode(token), token: res.locals.token })
-  } catch (error) {
-    throw error
-  }
-}
 
 module.exports = {
-  GetProfile,
   CreateUser,
-  SignInUser,
-  RefreshSession
 }
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,5 +1,9 @@
 const Router = require('express').Router()
 const AuthController = require('../controllers/AuthController')
 
+// Auth Routes
+Router.get('/signup', (req,res) => res.render('auth/signup'))
+Router.get('/login', (req,res) => res.render('auth/login'))
+Router.post('/signup', AuthController.CreateUser)
 
 module.exports = Router
